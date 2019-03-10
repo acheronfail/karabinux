34633551872da8b59268184fd062cc11d1c72e10
acheronfail <acheronfail@gmail.com>
2019-03-10 23:29:52 +1100

    docs: explain build.rs file

--- build.rs
+++ build.rs
@@ -1,7 +1,14 @@
 use std::env;
 
 pub fn main() {
-    // Allow conditional compilation in debug mode.
+    // Allow conditional compilation in different modes, for example:
+    //
+    // ```rust
+    // #[cfg(debug)]
+    // println!("I'm in debug mode!");
+    // #[cfg(release)]
+    // println!("I'm in release mode!");
+    // ```
     if let Ok(profile) = env::var("PROFILE") {
         println!("cargo:rustc-cfg={}", profile);
     }
