3d4c7cd08ea0dff9218604501b4c9e7397655b05
acheronfail <acheronfail@gmail.com>
2019-03-18 19:07:32 +1100

    chore: add KEY_CAPSLOCK to ALL_MODIFIER_KEYS

--- karabinux/src/constants.rs
+++ karabinux/src/constants.rs
@@ -2,8 +2,8 @@ use evdev_rs::enums::EV_KEY;
 
 pub const KARABINUX_DEVICE_NAME: &str = "KarabinuxDevice: ";
 
-pub const ALL_MODIFIER_KEYS: [EV_KEY; 8] = [
-    // EV_KEY::KEY_CAPSLOCK, // TODO: add this as a modifier???
+pub const ALL_MODIFIER_KEYS: [EV_KEY; 9] = [
+    EV_KEY::KEY_CAPSLOCK,
     EV_KEY::KEY_LEFTALT,
     EV_KEY::KEY_RIGHTALT,
     EV_KEY::KEY_LEFTMETA,
